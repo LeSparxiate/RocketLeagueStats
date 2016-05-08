9aae3da5a036e25deb13773cb09af5a22f7ec9c5
Thibault Desplat <despla_t@epitech.eu>
2016-05-08 20:50:13 +0200

    basic api functions with small WPF interface

---
+++ Workshop1/Workshop1/Classes/HttpRequest.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Workshop1.Classes
+{
+    /// <summary>
+    /// Used to perform HTTP requests. (POST/GET/...)
+    /// </summary>
+    public class HttpRequest
+    {
+        /// <summary>
+        /// Perform a POST request
+        /// </summary>
+        /// <param name="link">Request link</param>
+        /// <param name="parameters">Parameters</param>
+        /// <param name="headers">Header parameters</param>
+        /// <param name="h_data">Header values</param>
+        /// <returns>Result of the request</returns>
+        public static HttpWebResponse PostRequest(string link, string[] parameters = null, string[] headers = null, string[] h_data = null)
+        {
+            //string ret;
+            var postData = "";
+
+            var request = (HttpWebRequest)WebRequest.Create(link);
+
+            if (parameters != null)
+            {
+                for (int i = 0; i < parameters.Count(); i++)
+                {
+                    if (i > 0)
+                        postData += "&";
+                    postData += parameters[i];
+                }
+            }
+
+            var data = Encoding.ASCII.GetBytes(postData);
+            request.ContentLength = data.Length;
+
+            request.Method = "POST";
+            request.ContentType = "application/x-www-form-urlencoded";
+
+            if (headers != null && h_data != null)
+            {
+                for (int i = 0; i < headers.Count(); i++)
+                {
+                    request.Headers.Add(headers[i], h_data[i]);
+                }
+            }
+
+            using (var stream = request.GetRequestStream())
+            {
+                stream.Write(data, 0, data.Length);
+            }
+
+            var response = (HttpWebResponse)request.GetResponse();
+
+            return response;
+        }
+    }
+}
---
+++ Workshop1/Workshop1/Classes/RLApi.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Workshop1.Classes
+{
+    /// <summary>
+    /// Used to perform requests to RL API.
+    /// </summary>
+    public class RLApi
+    {
+        /// <summary>
+        /// Represent all Ranked GameTypes.
+        /// </summary>
+        public enum GameTypes
+        {
+            OVO = 10,
+            TVT = 11,
+            THVTHSOLO = 12,
+            THVTH = 13
+        }
+
+        public string sessionID = "";
+
+        /// <summary>
+        /// Allow to get a SessionID to request API.
+        /// </summary>
+        private void getSessionID()
+        {
+            string[] parameters = new string[] { "PlayerName=", "PlayerID=1", "Platform=PS4", "BuildID=812023742", "AuthCode=0", "IssuerID=0" };
+            string[] headers = new string[] { "LoginSecretKey" };
+            string[] h_data = new string[] { "dUe3SE4YsR8B0c30E6r7F2KqpZSbGiVx" };
+
+            HttpWebResponse ret = HttpRequest.PostRequest("https://psyonix-rl.appspot.com/auth/", parameters, headers, h_data);
+            this.sessionID = ret.Headers.Get("sessionid");
+        }
+
+        public string getPopulation()
+        {
+            string ret = "";
+
+            if (sessionID == "")
+                this.getSessionID();
+
+            string[] headers = new string[] { "SessionID", "CallProcKey", "Cache-Control", "Environment" };
+            string[] h_data = new string[] { sessionID, "pX9pn8F4JnBpoO8Aa219QC6N7g18FJ0F", "no-cache", "Prod" };
+
+            HttpWebResponse result = HttpRequest.PostRequest("https://psyonix-rl.appspot.com/Population/GetPopulation/", null, headers, h_data);
+
+            ret = new StreamReader(result.GetResponseStream()).ReadToEnd();
+
+            return ret;
+        }
+
+        /// <summary>
+        /// Allow to get all Top Ranking in specified GameType.
+        /// </summary>
+        /// <param name="gt">GameType : 1v1, 2v2, 3v3 solo, 3v3</param>
+        /// <returns>Response string</returns>
+        public string getAllRanking(GameTypes gt)
+        {
+            string ret = "";
+
+            if (sessionID == "")
+                this.getSessionID();
+
+            string[] parameters = new string[] { "Proc[]=GetSkillLeaderboard_v2", "P0P[]="+ (int)gt};
+            string[] headers = new string[] { "SessionID", "CallProcKey", "Cache-Control", "Environment" };
+            string[] h_data = new string[] { sessionID, "pX9pn8F4JnBpoO8Aa219QC6N7g18FJ0F", "no-cache", "Prod" };
+
+            HttpWebResponse result = HttpRequest.PostRequest("https://psyonix-rl.appspot.com/callproc105/", parameters, headers, h_data);
+
+            ret = new StreamReader(result.GetResponseStream()).ReadToEnd();
+
+            return ret;
+        }
+
+        /// <summary>
+        /// Allow to get a Steam User rank in specified GameType
+        /// </summary>
+        /// <param name="SteamPlayerID">Steam user ID</param>
+        /// <param name="gt">GameType : 1v1, 2v2, 3v3 solo, 3v3</param>
+        /// <returns>Response string</returns>
+        public string getPlayerRankingSteam(string SteamPlayerID, GameTypes gt)
+        {
+            string ret = "";
+
+            if (sessionID == "")
+                this.getSessionID();
+
+            string[] parameters = new string[] { "Proc[]=GetSkillLeaderboardValueForUser_v2Steam", "P0P[]="+SteamPlayerID, "P0P[]="+(int)gt };
+            string[] headers = new string[] { "SessionID", "CallProcKey", "Cache-Control", "Environment" };
+            string[] h_data = new string[] { sessionID, "pX9pn8F4JnBpoO8Aa219QC6N7g18FJ0F", "no-cache", "Prod" };
+
+            HttpWebResponse result = HttpRequest.PostRequest("https://psyonix-rl.appspot.com/callproc105/", parameters, headers, h_data);
+
+            ret = new StreamReader(result.GetResponseStream()).ReadToEnd();
+
+            return ret;
+        }
+    }
+}
